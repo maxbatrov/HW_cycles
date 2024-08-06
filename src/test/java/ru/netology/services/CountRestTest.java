5f3185228ff5086d7bc6eafb130b3ce05ae2bce2
Max Batrov <maxbatrov@gmail.com>
2024-08-06 23:50:25 +0200

    CSV commit

--- src/test/java/ru/netology/services/CountRestTest.java
+++ src/test/java/ru/netology/services/CountRestTest.java
@@ -2,28 +2,22 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class CountRestTest {
 
-    @Test
-    public void FirstCount() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/param.csv")
+    public void FirstCount(int expected, int income, int expenses, int threshold) {
         RestService service = new RestService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3000, 20_000);
+        //int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
-
-    @Test
-    public void SecondCount() {
-        RestService service = new RestService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+    
 }
